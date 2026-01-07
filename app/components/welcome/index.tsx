171b7aeb78693c31d1dc8b19fd6e43f7cf54086a
Yuqi Wu <78084089+helloigig@users.noreply.github.com>
2026-01-07 22:17:20 +0000

    Update index.tsx

--- app/components/welcome/index.tsx
+++ app/components/welcome/index.tsx
@@ -79,13 +79,13 @@ const Welcome: FC<IWelcomeProps> = ({
     notify({ type: 'error', message, duration: 3000 })
   }
 
-  const renderHeader = () => {
-    return (
-      <div className='absolute top-0 left-0 right-0 flex items-center justify-between border-b border-gray-100 mobile:h-12 tablet:h-16 px-8 bg-white'>
-        <div className='text-gray-900'>{conversationName}</div>
-      </div>
-    )
-  }
+  // const renderHeader = () => {
+  //   return (
+  //     <div className='absolute top-0 left-0 right-0 flex items-center justify-between border-b border-gray-100 mobile:h-12 tablet:h-16 px-8 bg-white'>
+  //       <div className='text-gray-900'>{conversationName}</div>
+  //     </div>
+  //   )
+  // }
 
   const renderInputs = () => {
     return (
